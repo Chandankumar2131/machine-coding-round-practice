3b0ac1410de797afdc8acf5c5b1cdef847ead0e7
Chandan Kumar <chandansinghrkt123@gmail.com>
2026-01-03 23:26:26 +0530

    first commit

---
+++ src/Acordion/Acordion.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Acordion({items}) {
+  console.log(items);
+  
+  return (
+    <div>Acordion</div>
+  )
+}
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Acordion from './Acordion/Acordion'
+import Item from './Acordion/Item'
+export default function App() {
+  return (
+   <Item />
+  )
+}
