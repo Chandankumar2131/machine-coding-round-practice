487f161ce6bcbf4a3fbf2e0ccdb06940aed021a5
Chandan Kumar <chandansinghrkt123@gmail.com>
2026-01-04 00:33:20 +0530

    created Acordian

--- src/Acordion/Acordion.jsx
+++ src/Acordion/Acordion.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Acordion({items}) {
-  console.log(items);
-  
+const [isOpen, setIsOpen]=useState(null);
+const handleClick =(index)=>{
+setIsOpen(isOpen==index?null:index)
+}
   return (
-    <div>Acordion</div>
+     !items || items.length===0 ? <div>No items to display</div> :
+    <div>
+      {items.map((item,index)=>{
+        return(
+          <div key={index}>
+            <button onClick={()=>handleClick(index)}>{item.title}</button>
+            {isOpen==index&& <div>{item.content}</div>}
+          </div>
+        )
+      })}
+    </div>
   )
 }
